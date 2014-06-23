48d3ceb4f87a4178ec067ab6a53ba3f5c78b4f6b
learnin9 <learnin9@gmail.com>
2014-06-23 19:30:44 +0800

    Create ready_install.sh

---
+++ ready_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DNS_FILE="resolv.conf"
+PIPFILE="pip.conf"
+EASY_INSTALL=".pydistutils.cfg"
+#Install Files in System
+cp -f $DNS_FILE /etc/
+mkdir -p ~/.pip/
+cp -f $PIPFILE ~/.pip/
+cp -f .pydistutils.cfg ~
+cp -f install-freeswitch.sh /usr/src
+cp -f install-newfies.sh /usr/src
+cp -f newfies-dialer-functions.sh /usr/src
